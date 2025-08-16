a7f638c8b0604fc58de68d06392d5c2491b2e58b
jobaydulcse <jobaydul.cse@gmail.com>
2025-08-16 18:10:37 +0600

    for of loop practice

---
+++ matched.js
@@ -0,0 +1,9 @@
+const numbers = [45, 65, 35, 75, 15, 95, 55];
+// for (let i = 0; i < numbers.length; i++) {
+//   const number = numbers[i];
+//   console.log(number);
+// }
+
+for (const number of numbers) {
+  console.log(number);
+}
